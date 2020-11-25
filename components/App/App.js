c11853449cd8ad944c741472641fd91e8506539c
yaron nachshon <yaronn@wix.com>
2020-11-26 01:46:35 +0200

    working poc

--- components/App/App.js
+++ components/App/App.js
@@ -1,13 +1,38 @@
+import React, { useState } from 'react';
 import Page from '../Page/Page';
 import Search from '../Search/Search';
 import Container from '@material-ui/core/Container';
+import Stage from '../Stage/Stage';
+
 
 const App = () => {
-    return (<Page>
-        <Container>
-            <Search />
-        </Container>
-    </Page>)
+    const [parasha, setParasha] = useState("");
+    const onChange = (e, value) => {
+        setParasha(value);
+    };
+    return (
+        <Page>
+            <Container dir="rtl">
+                <div className="search">
+                    <Search onChange={onChange} />
+                </div>
+                <div>
+                    <Stage parasha={parasha} />
+                </div>
+
+            </Container>
+
+            <style jsx>
+                {
+                    `
+            .search {
+                display: flex;
+                justify-content: center;
+            }
+            `}
+
+            </style>
+        </Page>)
 }
 
 export default App;
---
+++ components/Stage/Stage.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import tora from "../../consts/tora.json";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "100%",
+        maxWidth: "36ch",
+        backgroundColor: theme.palette.background.paper
+    },
+    inline: {
+        display: "inline"
+    }
+}));
+
+export default function AlignItemsList({ parasha }) {
+    const classes = useStyles();
+    if (!parasha) {
+        return null;
+    }
+    const pasuks = tora.parasha[parasha].stages[0];
+
+    return (
+        <List className={classes.root} a>
+            {pasuks.map((pasuk, index) => {
+                const parashaPath = tora.parasha[parasha].parashaLink.replace(
+                    "parasha.htm",
+                    ""
+                );
+                return (
+                    <>
+                        <div key={index} lignItems="center">
+                            <div key={index + "image"}>
+                                <img
+                                    style={{ maxWidth: "100%" }}
+                                    alt=""
+                                    src={`https://tora613.net/tora/${parashaPath}/${pasuk.image}`}
+                                />
+                            </div>
+                            <div key={index + "audio"}>
+                                <audio controls autoplay hidden="" style={{ width: "300px" }}>
+                                    <source
+                                        src={`https://tora613.net/tora/${parashaPath}${pasuk.audio}`}
+                                        type="audio/mpeg"
+                                    />
+                                </audio>
+                            </div>
+                        </div>
+                        <Divider variant="inset" component="li" />
+                    </>
+                );
+            })}
+        </List>
+    );
+}
