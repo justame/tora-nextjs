53cef334f948b44b5febfbd8757cb8e04917aef8
yaron nachshon <yaronn@wix.com>
2020-11-26 11:20:00 +0200

    eslint

--- components/Player/Player.js
+++ components/Player/Player.js
@@ -1,74 +1,100 @@
-import React, { useState, useEffect } from 'react'
-import styles from './Player.module.scss';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect, useRef } from 'react';
 import Button from '@material-ui/core/Button';
 import ReactAudioPlayer from 'react-audio-player';
+import styles from './Player.module.scss';
+
 const Player = ({ playlist }) => {
-    const [currentIndex, setCurrentIndex] = useState(-1);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const audioPlayerElement = useRef(null);
 
-    const startPlay = () => {
-
+  const onPasukEnd = () => {
+    if (currentIndex + 1 <= playlist.length - 1) {
+      setCurrentIndex(currentIndex + 1);
     }
+  };
 
-    const onPasukEnd = () => {
-        if (currentIndex + 1 <= playlist.length - 1) {
-            setCurrentIndex(currentIndex + 1)
-
-        }
+  const next = () => {
+    if (currentIndex + 1 <= playlist.length - 1) {
+      setCurrentIndex(currentIndex + 1);
     }
+  };
 
-    const next = () => {
-        if (currentIndex + 1 <= playlist.length - 1) {
-            setCurrentIndex(currentIndex + 1)
-        }
+  const back = () => {
+    if (currentIndex - 1 >= 0) {
+      setCurrentIndex(currentIndex - 1);
     }
+  };
 
-    const back = () => {
-        if (currentIndex - 1 >= 0) {
-            setCurrentIndex(currentIndex - 1)
-        }
+  const pause = () => {
+    if (audioPlayerElement.current) {
+      audioPlayerElement.current.audioEl.current.pause();
     }
+  };
 
-    useEffect(() => {
-        setCurrentIndex(0)
-    }, [playlist])
-
-    if (currentIndex < 0) {
-        return null;
+  const play = () => {
+    if (audioPlayerElement.current) {
+      audioPlayerElement.current.audioEl.current.play();
     }
-    const currentItem = playlist[currentIndex];
+  };
 
-    if (!currentItem) {
-        return null;
-    }
+  useEffect(() => {
+    setCurrentIndex(0);
+  }, [playlist]);
 
-    return (
-        <div className={styles.player}>
-            <div className={styles.image}>
-                <img src={currentItem.image} />
-            </div>
-            <div className={styles.audio}>
-                <ReactAudioPlayer
-                    src={currentItem.audio}
-                    controls
-                    autoPlay
-                    onEnded={onPasukEnd}
-                />
-            </div>
-            <div className={styles.caption}>
-                פסוק{currentIndex + 1} מתוך {playlist.length}
-            </div>
-            {<div>
-                <Button variant="contained" color="primary" onClick={back}>
-                    אחורה
-                </Button>
+  if (currentIndex < 0) {
+    return null;
+  }
+  const currentItem = playlist[currentIndex];
+
+  if (!currentItem) {
+    return null;
+  }
+
+  return (
+    <div className={styles.player}>
+      <div className={styles.image}>
+        <img src={currentItem.image} />
+      </div>
+      <div className={styles.audio}>
+        <ReactAudioPlayer
+          ref={audioPlayerElement}
+          src={currentItem.audio}
+          controls
+          autoPlay
+          onEnded={onPasukEnd}
+        />
+      </div>
+      <div className={styles.caption}>
+        פסוק
+        {currentIndex + 1}
+        {' '}
+        מתוך
+        {' '}
+        {playlist.length}
+      </div>
+      <div>
+        <Button variant="contained" color="primary" onClick={pause}>
+          הפסק
+        </Button>
+                &#160;
+        <Button variant="contained" color="primary" onClick={play}>
+          המשך
+        </Button>
+                &#160;
+        <Button variant="contained" color="primary" onClick={back}>
+          פסוק אחורה
+        </Button>
                 &#160;
 
-                <Button variant="contained" color="primary" onClick={next}>
-                    קדימה
-                </Button>
+        <Button variant="contained" color="primary" onClick={next}>
+          פסוק הבא
+        </Button>
 
-            </div>}
-        </div>)
-}
+      </div>
+    </div>
+  );
+};
 
 export default Player;
--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,29 +1,34 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-use-before-define */
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import tora from "../../consts/tora.json";
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { matchSorter } from 'match-sorter';
+import tora from '../../consts/tora.json';
+
+const filterOptions = (options, { inputValue }) => matchSorter(options, inputValue);
 
 export default function Search({ onChange }) {
-    return (
-        <div style={{ width: 300 }} dir="rtl">
-            <Autocomplete
-                dir="rtl"
-                id="free-solo-2-demo"
-                disableClearable
-                onChange={onChange}
-                options={Object.keys(tora.parasha).map((option) => option)}
-                renderInput={(params) => (
-                    <TextField
-                        dir="rtl"
-                        {...params}
-                        label="חפש פרשה"
-                        margin="normal"
-                        variant="outlined"
-                        InputProps={{ ...params.InputProps, type: "search" }}
-                    />
-                )}
-            />
-        </div>
-    );
+  return (
+    <div style={{ width: 300 }} dir="rtl">
+      <Autocomplete
+        dir="rtl"
+        id="free-solo-2-demo"
+        disableClearable
+        onChange={onChange}
+        options={Object.keys(tora.parasha).map((option) => option)}
+        filterOptions={filterOptions}
+        renderInput={(params) => (
+          <TextField
+            dir="rtl"
+            {...params}
+            label="חפש פרשה"
+            margin="normal"
+            variant="outlined"
+            InputProps={{ ...params.InputProps, type: 'search' }}
+          />
+        )}
+      />
+    </div>
+  );
 }
